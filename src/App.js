dc944ff333b76c537a570f95d0d9cde811666d5a
Felipe Bassi <fkb5105@psu.edu>
2023-12-07 14:38:10 -0500

    fixed the navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Resume from './components/Resume';
-import Gallery from './components/Gallery';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';  // Update the import to match the actual filename
+import Resume from './pages/Resume';
+import Gallery from './pages/Gallery';
 import Navigation from './components/Navigation';
 import './index.css';
 
@@ -13,9 +13,13 @@ const App = () => {
       <div>
         <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/gallery" element={<Gallery />} />
+          <Route
+            path="/"
+            element={<Navigate to="/pages/home" />}
+          />
+          <Route path="/pages/home" element={<Home />} />
+          <Route path="/pages/resume" element={<Resume />} />
+          <Route path="/pages/gallery" element={<Gallery />} />
         </Routes>
       </div>
     </Router>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 // Navigation.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navigation.css';
+import '../styles/Navigation.css'; 
 
 const Navigation = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -15,13 +15,13 @@ const Navigation = () => {
       <div className="burger-icon" onClick={toggleMenu}>&#9776;</div>
       <ul className={`nav-list ${isMenuOpen ? 'open' : ''}`}>
         <li className="nav-item">
-          <Link to="/">Home</Link>
+          <Link to="/pages/home">Home</Link>
         </li>
         <li className="nav-item">
-          <Link to="/resume">Resume</Link>
+          <Link to="/pages/resume">Resume</Link>
         </li>
         <li className="nav-item">
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/pages/gallery">Gallery</Link>
         </li>
       </ul>
     </nav>
