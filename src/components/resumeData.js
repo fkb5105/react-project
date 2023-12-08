768647d58d93b8c6664ff641247fff040058a7c2
Felipe Bassi <fkb5105@psu.edu>
2023-12-08 10:30:33 -0500

    resume page

---
+++ src/components/resumeData.js
@@ -0,0 +1,68 @@
+const resumeData = {
+
+  "education": {
+    "University": ["The Pennsylvania State University"],
+    "Graduation": ["May 2024"],
+    "Major": ["Digital Media, Arts and Technology, Bachelor of Arts"],
+    "Focus Area": ["Data Visualization and Assessment"],
+    "Minors": ["Data Visualization", "Management Information Systems"],
+    "Awards": ["PSU Honors Program Certificate | Dean’s List Fall 2020 – Spring 2023"],
+    "GPA": ["3.55"]
+  },
+  "experience": [
+    {
+      "title": "Data Visualization Technician - Penn State IT Department",
+      "date": "June 2023 - Present",
+      "responsibilities": [
+        "Identified business requirements and developed visualizations accordingly.",
+        "Transformed raw data into meaningful and insightful analysis.",
+        "Built data models with Power BI and created relationships between multiple data sources.",
+        "Used Data Analysis Expressions to solve data analysis problems.",
+        "Generated queries with SQL to extract and adjust specific data values and columns."
+      ]
+    },
+    {
+      "title": "Applications Specialist - VAR Labs",
+      "date": "October 2022 – August 2023",
+      "responsibilities": [
+        "Programmed with React Native and TypeScript.",
+        "Utilized RESTful APIs and integrated them into the app.",
+        "Worked collaboratively in a team environment and communicated effectively.",
+        "Experienced agile development methodologies and project management tools.",
+        "Created databases and integrated them into the application's data flow."
+      ]
+    },
+    {
+      "title": "Website Developer - Penn State University",
+      "date": "May 2022 – August 2022",
+      "responsibilities": [
+        "Led a team of 4 students to build the new Penn State's Digital Media program website.",
+        "Designed an efficient and user-friendly UI.",
+        "Ensured website compatibility across multiple platforms and devices through responsive design.",
+        "Conducted user testing and analyzed website analytics to optimize user experience."
+      ]
+    }
+  ],
+  "projects": [
+    {
+      "title": "Erie Insurance Webpage Redesign – DIGIT 400, Digital Project Design",
+      "date": "September 2023 - October 2023",
+      "responsibilities": [
+        "Designed an improved website structure for navigation and access to insurance terms from the dictionary page.",
+        "Applied graphics and search engine optimization to improve the reading experience.",
+        "Rewrote, reorganized, and grouped information on the page to optimize connecting related terms.",
+        "Ensured website compatibility across multiple platforms and devices through responsive design.",
+        "Conducted user testing and analyzed website analytics to optimize user experience."
+      ]
+    }
+  ],
+  "skills": {
+    "Data Analysis": ["Power BI", "IBM SPSS statistics", "Stat Tools", "Neural Tools"],
+    "Coding": ["HTML", "CSS", "XML", "SQL", "React Native", "JavaScript", "Python"],
+    "Database Management": ["MySQL", "Microsoft SQL server", "Access"],
+    "Languages": ["Spanish", "English"],
+    "Office365": ["Excel", "Teams", "Word"],
+    "Other Skills": ["GitHub", "Adobe Photoshop", "Adobe Premier", "Adobe XD", "Adobe Illustrator"]
+  }
+}
+export default resumeData;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,60 @@
 // Resume.js
 import React from 'react';
+import '../styles/resume.css';
+import resumeData from '../components/resumeData'
 
 const Resume = () => {
+  const { education, experience, projects, skills } = resumeData;
+
   return (
-    <div>
-      <h1>Resume Page</h1>
+    <div className="resume-container">
+      <section className="education">
+        <h2>Education</h2>
+        {Object.entries(education).map(([category, items]) => (
+          <div key={category}>
+            <p><b>{category}</b>: {items.join(', ')}</p>
+          </div>
+        ))}
+      </section>
+
+      <section className="experience">
+        <h2>Information Systems Development Experience</h2>
+        {experience.map((job) => (
+          <div key={job.title}>
+            <h3>{job.title}</h3>
+            <p>{job.date}</p>
+            <ul>
+              {job.responsibilities.map((responsibility, index) => (
+                <li key={index}>{responsibility}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
+
+      <section className="projects">
+        <h2>Relevant Projects</h2>
+        {projects.map((project) => (
+          <div key={project.title}>
+            <h3>{project.title}</h3>
+            <p>{project.date}</p>
+            <ul>
+              {project.responsibilities.map((detail, index) => (
+              <li key={index}>{detail}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
+
+      <section className="skills">
+        <h2>Skills</h2>
+        {Object.entries(skills).map(([category, items]) => (
+          <div key={category}>
+            <p><b>{category}</b>: {items.join(', ')}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
